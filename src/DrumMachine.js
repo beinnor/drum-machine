ae3e29133fa1726b0e5a31095870633d37dd6ac6
Ronnie <16521174+beinnor@users.noreply.github.com>
2019-07-18 23:42:28 +0200

    workspace setup finished

---
+++ src/DrumMachine.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import './DrumMachine.css';
+
+class DrumMachine extends React.Component {
+  render() {
+    return (
+      <div className="DrumMachine">
+        <h1>Drum Machine</h1>
+      </div>
+    );
+  }
+}
+
+export default DrumMachine;
