50daa084bbb33802acc53eb6c911f03c89da1adb
Ronnie <16521174+beinnor@users.noreply.github.com>
2019-07-18 22:48:05 +0200

    workspace setup

--- src/App.js
+++ src/App.js
@@ -1,11 +1,15 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import logo from "./logo.svg";
+import "./App.css";
 
 function App() {
+  const x = 2;
+  x = 23;
+
   return (
     <div className="App">
       <header className="App-header">
+        {" "}
         <img src={logo} className="App-logo" alt="logo" />
         <p>
           Edit <code>src/App.js</code> and save to reload.
