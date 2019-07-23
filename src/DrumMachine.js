95bddc2eb33ab89500e4ff01eb85983a5bd1b65b
Ronnie <16521174+beinnor@users.noreply.github.com>
2019-07-23 22:31:26 +0200

    first commit with real code

--- src/DrumMachine.js
+++ src/DrumMachine.js
@@ -1,11 +1,13 @@
 import React from 'react';
-import './DrumMachine.css';
+import DrumPad from './DrumPad';
 
 class DrumMachine extends React.Component {
   render() {
     return (
-      <div className="DrumMachine">
+      <div className="DrumMachine" id="drum-pad">
         <h1>Drum Machine</h1>
+        <div id="display">Display</div>
+        <DrumPad />
       </div>
     );
   }
---
+++ src/DrumPad.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import './DrumPad.css';
+
+class DrumPad extends React.Component {
+  render() {
+    return (
+      <div className="drum-pad">
+        <h1>DrumPad</h1>
+      </div>
+    );
+  }
+}
+
+export default DrumPad;
