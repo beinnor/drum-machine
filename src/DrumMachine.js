b3e4de3ab6d13be01175b433c6e7cad6600c2e56
Ronnie <16521174+beinnor@users.noreply.github.com>
2019-07-26 21:43:18 +0200

    seperated drumbanks in own file

--- src/DrumMachine.js
+++ src/DrumMachine.js
@@ -1,68 +1,13 @@
 import React from 'react';
 import DrumPad from './DrumPad';
-
-const bankOne = [
-  {
-    keyCode: 81,
-    keyTrigger: 'Q',
-    id: 'Heater-1',
-    url: 'https://s3.amazonaws.com/freecodecamp/drums/Heater-1.mp3'
-  },
-  {
-    keyCode: 87,
-    keyTrigger: 'W',
-    id: 'Heater-2',
-    url: 'https://s3.amazonaws.com/freecodecamp/drums/Heater-2.mp3'
-  },
-  {
-    keyCode: 69,
-    keyTrigger: 'E',
-    id: 'Heater-3',
-    url: 'https://s3.amazonaws.com/freecodecamp/drums/Heater-3.mp3'
-  },
-  {
-    keyCode: 65,
-    keyTrigger: 'A',
-    id: 'Heater-4',
-    url: 'https://s3.amazonaws.com/freecodecamp/drums/Heater-4_1.mp3'
-  },
-  {
-    keyCode: 83,
-    keyTrigger: 'S',
-    id: 'Clap',
-    url: 'https://s3.amazonaws.com/freecodecamp/drums/Heater-6.mp3'
-  },
-  {
-    keyCode: 68,
-    keyTrigger: 'D',
-    id: 'Open-HH',
-    url: 'https://s3.amazonaws.com/freecodecamp/drums/Dsc_Oh.mp3'
-  },
-  {
-    keyCode: 90,
-    keyTrigger: 'Z',
-    id: "Kick-n'-Hat",
-    url: 'https://s3.amazonaws.com/freecodecamp/drums/Kick_n_Hat.mp3'
-  },
-  {
-    keyCode: 88,
-    keyTrigger: 'X',
-    id: 'Kick',
-    url: 'https://s3.amazonaws.com/freecodecamp/drums/RP4_KICK_1.mp3'
-  },
-  {
-    keyCode: 67,
-    keyTrigger: 'C',
-    id: 'Closed-HH',
-    url: 'https://s3.amazonaws.com/freecodecamp/drums/Cev_H2.mp3'
-  }
-];
+import { bankTwo as drumBank } from './utils/drumBanks';
 
 class DrumMachine extends React.Component {
   constructor(props) {
     super(props);
     this.state = {
-      display: ''
+      display: '',
+      currentBank: drumBank
     };
   }
 
@@ -71,7 +16,7 @@ class DrumMachine extends React.Component {
   };
 
   render() {
-    const padBank = bankOne.map(bank => {
+    const padBank = this.state.currentBank.map(bank => {
       return (
         <DrumPad key={bank.id} data={bank} updateDisplay={this.updateDisplay} />
       );
