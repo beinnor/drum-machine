314dc345b91aa9e2ddd7e721de63b5a2bc73cd7c
Ronnie <16521174+beinnor@users.noreply.github.com>
2019-08-02 01:20:21 +0200

    added feature

--- src/Controls.js
+++ src/Controls.js
@@ -1,20 +1,61 @@
 import React from 'react';
 import './Controls.css';
 
-export default function Controls(props) {
-  return (
-    <div className="controls">
-      <div className="nextBankBtn" onClick={props.nextBank}>
-        BANK
+export default class Controls extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      btnClassName: 'nextBankBtn'
+    };
+    this.handleKeypress = this.handleKeypress.bind(this);
+    this.handleClick = this.handleClick.bind(this);
+    this.lightUpPad = this.lightUpPad.bind(this);
+  }
+
+  componentDidMount() {
+    document.addEventListener('keydown', this.handleKeypress);
+  }
+  componentWillUnmount() {
+    document.removeEventListener('keydown', this.handleKeypress);
+  }
+
+  handleKeypress(e) {
+    if (e.keyCode === 66) {
+      this.props.nextBank();
+      this.lightUpPad();
+      setTimeout(() => this.lightUpPad(), 100);
+    }
+  }
+
+  handleClick() {
+    this.lightUpPad();
+    setTimeout(() => this.lightUpPad(), 100);
+    this.props.nextBank();
+  }
+
+  lightUpPad = () => {
+    if (this.state.btnClassName === 'nextBankBtn') {
+      this.setState({ btnClassName: 'nextBankBtn active' });
+    } else {
+      this.setState({ btnClassName: 'nextBankBtn' });
+    }
+  };
+
+  render() {
+    return (
+      <div className="controls">
+        <div className={this.state.btnClassName} onClick={this.handleClick}>
+          BANK
+        </div>
+        <div>
+          <input
+            type="text"
+            className="currentBankDisplay"
+            value={this.props.currentBank[0]}
+            disabled
+          />
+        </div>
       </div>
-      <div>
-        <input
-          type="text"
-          className="currentBankDisplay"
-          value={props.currentBank[0]}
-          disabled
-        />
-      </div>
-    </div>
-  );
+    );
+  }
 }
